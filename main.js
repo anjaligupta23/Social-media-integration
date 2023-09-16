868333969363238a005784fbba16f5bf921f368e
anjaligupta23 <127794305+anjaligupta23@users.noreply.github.com>
2023-09-16 14:40:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ['Frontend Developer','Web developer', 'Full Stack Developer','Coder','Programmer','Keen Learner'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+  
